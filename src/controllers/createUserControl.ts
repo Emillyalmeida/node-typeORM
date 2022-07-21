ff8653bc0fe3c1a249f848183cbf8f94a7387f37
emillyalmeida <emillyalmeidan@gmail.com>
2022-07-21 13:10:43 -0300

    feat: add verificantios name email

--- src/controllers/createUserControl.ts
+++ src/controllers/createUserControl.ts
@@ -1,8 +1,19 @@
-import { Response, Request } from "express";
+import { Response, Request, response } from "express";
+import CreateUserService from "../services/createUserService";
 
 class CreateUserControl {
-  static handler(req: Request, res: Response): Promise<Response> {
+  static handler(req: Request, res: Response) {
     const { name, email } = req.body;
+
+    const createUserService = new CreateUserService();
+
+    if (!name || !email) {
+      return response.status(400).json({ message: "informe todos os campos" });
+    }
+
+    const users = createUserService.execute({ name, email });
+
+    return response.status(201).json({ user: { name, email } });
   }
 }
 
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from "express";
 import routeUser from "./routes/userRoutes";
+import "reflect-metadata";
 
 const app = express();
 app.use(express.json());
