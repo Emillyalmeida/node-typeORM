8eea461702da0d91b2f0e2557f5d2cf78ab70629
emillyalmeida <emillyalmeidan@gmail.com>
2022-07-21 22:45:29 -0300

    feat: create migration to users

---
+++ src/db/1658424480110-migrations.ts
@@ -0,0 +1,34 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class migrations1658424480110 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: "users",
+        columns: [
+          {
+            name: "id",
+            type: "int",
+            isPrimary: true,
+            isNullable: false,
+          },
+          {
+            name: "name",
+            type: "varchar",
+            isNullable: false,
+          },
+          {
+            name: "email",
+            type: "varchar",
+            isNullable: true,
+          },
+        ],
+      }),
+      true
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("users");
+  }
+}
